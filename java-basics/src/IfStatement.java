da82c2b9430f4dbda4dc1e88be7db8194afc1b2b
Michał Gomoła <michal.gomola.89@gmail.com>
2022-12-12 22:10:00 +0100

    IfStatement lesson added

---
+++ java-basics/src/IfStatement.java
@@ -0,0 +1,8 @@
+public class IfStatement {
+    public static void main(String[] args){
+        boolean isCold = true;
+        if(isCold) {
+            System.out.println("It's cold outside, wear a coat!");
+        }
+    }
+}
