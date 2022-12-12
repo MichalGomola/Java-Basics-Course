adcc9eb8c3e44271fad20d22fb9367233006f3cd
Michał Gomoła <michal.gomola.89@gmail.com>
2022-12-12 17:39:18 +0100

    String lessons added

---
+++ untitled/src/StringVariables.java
@@ -0,0 +1,11 @@
+public class StringVariables {
+    public static void main(String[] args) {
+        String driver = "Mikele";
+        System.out.println(driver);
+        int letters = driver.length();
+        System.out.println(letters);
+        driver = driver.toUpperCase();
+        System.out.println(driver);
+        
+    }
+}
