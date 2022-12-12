1a3cb11368c685972f157a8fa5fcd89a6de00d2d
Michał Gomoła <michal.gomola.89@gmail.com>
2022-12-12 20:30:31 +0100

    VariableTypes lesson added

---
+++ untitled/src/VariableTypes.java
@@ -0,0 +1,11 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        int maxInt = 2147483647;
+        long muchMore = 2147483647*10000000L;
+        double fraction = 99.123;
+        String fullText = "Some stuff in here";
+        char answer = 'a';
+        boolean fact = true; //or false
+        
+    }
+}
