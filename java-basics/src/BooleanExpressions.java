7c7e4de18e8abb49e855de73c0780d8a7b66ba07
Michał Gomoła <michal.gomola.89@gmail.com>
2022-12-12 22:53:12 +0100

    BooleanExpressions lessons added

---
+++ java-basics/src/BooleanExpressions.java
@@ -0,0 +1,10 @@
+public class BooleanExpressions {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = 5 > 3;
+        boolean b4 = 3 > 5;
+        boolean b5 = 7 <=6;
+
+    }
+}
