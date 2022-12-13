2c45146ff6c4f11263df306cd03a8481efa0586d
Michał Gomoła <michal.gomola.89@gmail.com>
2022-12-13 20:24:50 +0100

    FunctionDefinition lesson added

---
+++ java-basics/src/FunctionDefinition.java
@@ -0,0 +1,13 @@
+public class FunctionDefinition {
+    public static void main(String[] args) {
+        chorus();
+        chorus();
+    }
+
+    public static void chorus() {
+        System.out.println("Once I had a love and it was a gas");
+        System.out.println("Soon turned out had a heart of glass");
+        System.out.println("Seemed like the real thing, only to find");
+        System.out.println("Mucho mistrust, love's gone behind");
+    }
+}
