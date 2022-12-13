dea114ec17a15f9162314ba605eacf9684dbcab1
Michał Gomoła <michal.gomola.89@gmail.com>
2022-12-13 22:16:42 +0100

    Random lesson updated

--- java-basics/src/RandomNumbers.java
+++ java-basics/src/RandomNumbers.java
@@ -1,8 +1,14 @@
 public class RandomNumbers {
     public static void main(String[] args) {
-        System.out.print(rollDice());
+        System.out.print(rollDice(6));
     }
-    public static int rollDice() {
-        return (int) (Math.random()*6+1); // returns value between 1 and 6
+    /*
+    * rollDice function makes a dice roll
+    * @param sides - the number of sides dice have (int)
+    *
+    * @return random roll value (int)
+     */
+    public static int rollDice(int sides) {
+        return (int) (Math.random()*sides+1); // returns value between 1 and 6
     }
 }
